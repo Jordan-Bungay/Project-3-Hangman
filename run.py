d00e7e4c6003945d443a8d126db6298b5e97f268
Jordan-Bungay <>
2023-12-27 22:02:44 +0000

    Add Introduction message to the game

--- run.py
+++ run.py
@@ -12,8 +12,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('name_sheet')
 
-names = SHEET.worksheet('names')
 
-data = names.get_all_values()
+def Introduction():
+    """
+    Message to appear when game is started.
+    Instructions on how to play.
+    """
+    print("Welcome to the letter lair heh... heh... heh...")
+    print("Where we play a little game called HANGMAN")
+    print("Where you gotta find the hidden word or else...")
+    print("You better win man or you gonna get hanged man... HaHaHaHA!")
+    print()
+    print("I'll give you a little info before we start:")
+    print()
+    print("1. Underscores signify each letter that makes up the hidden word.")
+    print("2. Type a letter to start guessing the word.")
+    print("3. Correct letters guessed, reveal all letter(s) in the word.")
+    print("4. Each wrong guess and I will start to set up the HANGMAN he he")
+    print("5. 6 incorrect guesses, you can guess what happens... (you lose).")
+    print("6. Enjoy! he he he...")
 
-print(data)
+
+Introduction()
